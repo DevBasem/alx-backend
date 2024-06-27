d6b6d03760706902e85e935e18b099f723ee4f8d
DevBasem <engr.basemahmed@gmail.com>
2024-06-26 18:51:04 -0700

    task 4

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -2,6 +2,7 @@
 """ MRUCache module """
 from base_caching import BaseCaching
 
+
 class MRUCache(BaseCaching):
     """ MRUCache inherits from BaseCaching and implements MRU caching """
 
@@ -14,7 +15,6 @@ class MRUCache(BaseCaching):
         """ Add an item in the cache """
         if key is not None and item is not None:
             if len(self.cache_data) >= self.MAX_ITEMS:
-                # MRU eviction: remove the most recently used item (last item in queue)
                 if self.queue:
                     mru_key = self.queue.pop()
                     del self.cache_data[mru_key]
