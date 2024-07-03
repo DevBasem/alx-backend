476e46cd8b0c7d81ecd1fd4e0f5937b739c6c7e5
DevBasem <engr.basemahmed@gmail.com>
2024-07-03 10:30:10 -0700

    task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with Babel configuration
+"""
+
+from flask import Flask
+from flask_babel import Babel
+
+# Configuration class
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Instantiate Babel object
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    return "Hello, world!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
