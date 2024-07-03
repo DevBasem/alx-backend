7e50d3aa3b598197020705fa8367756df0082b01
DevBasem <engr.basemahmed@gmail.com>
2024-07-03 10:47:49 -0700

    task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,45 +1,55 @@
 #!/usr/bin/env python3
-'''Task 2: Get locale from request
-'''
+"""
+Flask app with Babel configuration and locale selection.
+"""
 
-from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask import Flask, request, render_template
+from flask_babel import Babel, gettext
 
 
+# Configuration class
 class Config:
-    '''Config class'''
+    """
+    Configuration class for Flask app.
 
-    DEBUG = True
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    Attributes:
+        LANGUAGES (list): List of available languages for localization.
+        BABEL_DEFAULT_LOCALE (str): Default locale for Babel, set to 'en'.
+        BABEL_DEFAULT_TIMEZONE (str): Default timezone for Babel, set to 'UTC'.
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
+
+# Instantiate Babel object
 babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale() -> str:
-    """Retrieves the locale for a web page.
+def get_locale():
+    """
+    Determine the user's locale based on the request's accepted languages.
 
     Returns:
-        str: best match
+        str: The best matching language code from the supported LANGUAGES.
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def index() -> str:
-    '''default route
+def index():
+    """
+    Route handler for the '/' route.
 
     Returns:
-        html: homepage
-    '''
-    return render_template("2-index.html")
+        str: Rendered HTML template.
+    """
+    return render_template('2-index.html')
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
