09c0d5a2c8453523a96e720353b164205c4b1ffe
DevBasem <engr.basemahmed@gmail.com>
2024-07-03 10:27:35 -0700

    task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,10 +7,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     '''basic route'''
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
